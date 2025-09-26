a81c51241ebb41f18b517e85049ab44793e652c0
stockton5 <stockton5@byui.edu>
2025-09-25 21:28:33 -0600

    prove03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+         int number;
+        
+         Random randomNumber = new Random();
+        int real = randomNumber.Next(1, 101);
+
+        do
+
+        {
+            Console.Write("What is the magic number?");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < real)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number > real)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+        while (number != real);
+        {
+
+        }
+        
+    
+
     }
+    
+
+   
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+
+
+        // loops
+        // foreach 
+        // List<string> fruits = new List<string>() { "apple", "banana", "cherry" };
+        // foreach (string fruit in fruits)
+
+        // {
+        //     Console.WriteLine(fruit);
+
+        // }
+
+        // fruits[0] = "Apricot";
+
+        // Console.WriteLine(fruits[0]);
+
+        // // for
+        // // in python: for i in range(0,10)
+
+        // for (int i = 0; i < 10; i++)
+
+        // {
+        //     Console.WriteLine($"i: {i}");
+        // }
+
+        // // while
+
+        // Console.Write("Make a choice Y or N: ");
+        // string value = Console.ReadLine().ToUpper();
+        // while (value != "Y" && value != "N")
+
+        // {
+        //     Console.Write("Choose y or n;");
+        //     value = Console.ReadLine().ToUpper();
+
+        // }
+
+        // do while
+        string value;
+        do
+        {
+            Console.Write("Enter Y or N");
+            value = Console.ReadLine().ToUpper();
+
+        } while (value != "Y" && value != "N");
+
+        // int[] myArray = new int[](1, 2, 3, 4, 5);
+        // Console.Write("Before");
+
+        // foreach (int )
+
     }
+
 }
